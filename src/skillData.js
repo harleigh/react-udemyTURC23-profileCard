1b23cedc9ffed6871a5dead2c005086cc6659107
harleigh <harleigh.marsh@gmail.com>
2023-09-18 12:18:21 -0700

    added levels to each skill

--- src/skillData.js
+++ src/skillData.js
@@ -1,19 +1,50 @@
 
 export const skillsData = [
-    {name: "JavaScript"},
-    {name: "React"},
-    {name: "Python"},
-    {name: "Matlab"},
-    {name: "Java"},
-    {name: "C"},
-    {name: "AutoHotKey"},
-    {name: "Git Github"},
-    {name: "Trajectory Optimization"},
-    {name: "Applied Mathematics"},
-    {name: "Scientific Writing"},
-    {name: "Technical Writing"},
-    {name: "LaTeX"},
-    {name: "Teaching"},
-    {name: "Curriculum Development"},
-    {name: "Course Content Creation"}
+    {name: "JavaScript",
+     level:"Advanced"},
+
+    {name: "React",
+    level:"Intermediate"},
+
+    {name: "Python",
+    level:"Advanced"},
+    
+    {name: "Matlab",
+    level:"Advanced"},
+    
+    {name: "Java",
+    level:"Advanced"},
+    
+    {name: "C",
+    level:"Intermediate"},
+    
+    {name: "AutoHotKey",
+    level:"Intermediate"},
+    
+    {name: "Git Github",
+    level:"Intermediate"},
+    
+    {name: "Trajectory Optimization",
+    level:"Advanced"},
+    
+    {name: "Applied Mathematics",
+    level:"Advanced"},
+    
+    {name: "Scientific Writing",
+    level:"Advanced"},
+    
+    {name: "Technical Writing",
+    level:"Advanced"},
+    
+    {name: "LaTeX",
+    level:"Advanced"},
+    
+    {name: "Teaching",
+    level:"Advanced"},
+    
+    {name: "Curriculum Development",
+    level:"Advanced"},
+    
+    {name: "Course Content Creation",
+    level:"Advanced"}
 ]
